f41e1a2bebfd4c1404b80638bc422635162f3fc1
Poul Rosenberg <poul@poulrosenberg.dk>
2021-10-24 17:51:57 +0200

    modified server.js

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-// Install express server
 const express = require('express');
+const serveStatic = require('serve-static');
 const path = require('path');
-const app = express();
-var PORT = 8080                         // default Heroku port
+var PORT = 8080
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist'));
-app.use(express.static(__dirname + '/dist/en'));
-// app.use(express.static(__dirname + '/dist/da'));
+app = express();
+app.use(serveStatic(path.join(__dirname,'dist')));
 
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/en/'}),
-);
-    
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || PORT);
